79afd9cc7869f7b0a385f43d1d06c6b238f5adf6
Deepshikha Sethi <deepshikha.sethi@zenatix.com>
2019-09-28 22:44:22 +0530

    Improved views logic

--- weather/views.py
+++ weather/views.py
@@ -5,8 +5,7 @@ import requests
 def home(request):
     api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=text/csv&zipCode=60176&distance=5&API_KEY=E5988E53-F48E-4474-BA18-F51F4245D669")
     try:
-        api = json.loads(api_request.content)
-        print(api)
+        api = api_request.content
     except Exception as e:
         api = "Error ..."
 
