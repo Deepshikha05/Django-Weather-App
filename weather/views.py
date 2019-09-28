776672b9d4e14e4f4f64e1e72eaa71b2d23a6485
Deepshikha Sethi <deepshikha.sethi@zenatix.com>
2019-09-28 22:40:06 +0530

    Changed Views Logic

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+import json 
+import requests
 
 def home(request):
-    return render(request, 'home.html', {})
+    api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=text/csv&zipCode=60176&distance=5&API_KEY=E5988E53-F48E-4474-BA18-F51F4245D669")
+    try:
+        api = json.loads(api_request.content)
+        print(api)
+    except Exception as e:
+        api = "Error ..."
+
+    return render(request, 'home.html', {'api':api})
 
 def about(request):
     return render(request, 'about.html', {})
